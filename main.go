8de7173db9e9213060827d7f45c406a90b8473cf
Dr3iundZwanzig <zaude.sven@gmail.com>
2026-05-15 17:30:49 +0200

    added initial HTML file and logo; set up basic file server

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	serveMux := http.NewServeMux()
 	port := ":8080"
+	filepath := "."
+
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepath))))
+	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(http.StatusText(http.StatusOK)))
+
+	})
+
 	server := http.Server{
 		Addr:    port,
 		Handler: serveMux,
 	}
+	fmt.Println("Server started at port" + port)
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
